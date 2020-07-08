e81bf45006efe0c8f4ee9d80741f7f71fabe84d9
leska0000 <leska0000@gmail.com>
2020-07-08 22:05:43 +0300

    num1

--- source/U-Jewelries/U-Jewelries_ClassLibrary/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace U_Jewelries_ClassLibrary.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- source/U-Jewelries/U-Jewelries_ClassLibrary/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace U_Jewelries_ClassLibrary.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
